8a9f9bae01649bcf0e1da8150a21bd01d97200f2
Valo Reyes <conspiringhumanity@gmail.com>
2019-02-10 23:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // write your code below!
 var name = "Susan"
 var height = 39
-var message = `${name} is ${height} inches tall`
+var message = `${name} is ${height} inches Giant`
+console.log(message)
 
 module.exports = {name, height, message}
